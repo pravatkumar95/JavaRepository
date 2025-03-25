3fb6a2a082739313fde1e4d6e4f12f2719323fe8
pravatkumar95 <pravatkumar2495@gmail.com>
2025-03-25 18:57:40 +0530

    message3 added

--- AuthService/src/main/java/com/pks/controller/AuthController.java
+++ AuthService/src/main/java/com/pks/controller/AuthController.java
@@ -31,6 +31,7 @@ public class AuthController {
     @GetMapping("/getStatus")
     public ResponseEntity<String> getStatus(){
         String message="Welcome to pravat";
+        String message3="welcome to centroxy";
         return new ResponseEntity<>(message,HttpStatus.OK);
     }
 
